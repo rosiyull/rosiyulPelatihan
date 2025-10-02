52d29a79fdd12380b80d4b1b79ead2729eace4eb
rosiyull <rosyyuuul444@gmail.com>
2025-10-02 12:09:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+const tombolSapa = document.getElementById('sapaButton');
+tombolSapa.addEventListener('click', function() {
+    alert('Halo call me rosiyul, selamat datang di profil saya!');
+});
